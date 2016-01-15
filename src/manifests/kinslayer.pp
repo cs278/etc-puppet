5b67b37fe7d8b9f23b4c9325753bcefe59a5e902
Chris Smith <chris@cs278.org>
2016-01-15 23:59:11 +0000

    NTP is helpful

--- src/manifests/kinslayer.pp
+++ src/manifests/kinslayer.pp
@@ -5,6 +5,10 @@ Exec {
 class { 'apt':
 }
 
+class { '::ntp':
+  servers => ['0.uk.pool.ntp.org', '1.uk.pool.ntp.org', '2.uk.pool.ntp.org'],
+}
+
 apt::source { 'google-chrome':
   location => 'http://dl.google.com/linux/chrome/deb/',
   release  => 'stable',
